2e2e1c1257d043d42272765bf979736484df4981
array-in-a-matrix <git@arrayinamatrix.xyz>
2024-07-04 14:33:25 -0400

    list all files api endpoint

--- src/routes/download.nim
+++ src/routes/download.nim
@@ -27,3 +27,28 @@ proc createDownloadRoutes*() =
         resp Http404, "File does not exist"
     
       sendFile file.path
+    #[ 
+      request parameters: 
+        token     -  string         -  required via header
+      returns:
+        success   -  JSON           -  JSON node containing all files and tags
+        fail      -  403            -  invalid token
+        fail      -  404            -  files doesn't exist
+    ]#
+    get "/api/v1/listAllFiles":
+      var user = newUser()
+      if not db.validToken(user, request.headers["Authorization"]):
+        resp Http403, "Invalid token."
+      
+      var listOfFiles = @[newFile()]
+      try:
+        db.select(listOfFiles, "File.owner = ?", user.id)
+      except NotFoundError:
+        resp Http404, "Files does not exist"
+
+      # TODO: make this use JsonNode type and respond with JSON
+      var allFiles: string
+      for file in listOfFiles:
+        allFiles = allFiles & '\n' & $file[]
+
+      resp Http200, allFiles
