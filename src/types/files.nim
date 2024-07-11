ba856038e2dbd24d2d87ea9aadd87b220a1f6085
array-in-a-matrix <git@arrayinamatrix.xyz>
2024-07-11 19:42:59 -0400

    test git hook

--- src/types/files.nim
+++ src/types/files.nim
@@ -8,7 +8,6 @@ type File* = ref object of Model
   name*: string
   tags*: string #? This is a temporary hack should be `seq[string]` or `JsonNode` instead
 
-              # creates a new file object and sets default values, recommended by the norm documentation
-func newFile*(user: User = newUser(), path: string = "", name: string = "",
-    tags: string = ""): File =
+# creates a new file object and sets default values, recommended by the norm documentation
+func newFile*(user: User = newUser(), path: string = "", name: string = "", tags: string = ""): File =
   File(owner: user, path: path, name: name, tags: tags)
