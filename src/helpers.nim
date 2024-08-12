6df0899491101fb808ff574684f599295b4204ec
array-in-a-matrix <git@arrayinamatrix.xyz>
2024-08-12 14:40:44 -0400

    log error and respond + request info

--- src/helpers.nim
+++ src/helpers.nim
@@ -1,2 +1,10 @@
 template H*(s: string): untyped =
   $request.headers[s]
+
+template reqInfo*: string =
+  $request.reqMethod & " " & request.host & request.path & " " & request.ip &
+      " " & $request.headers["user-agent"]
+
+template respErr*(s: string): untyped =
+  error s & reqInfo
+  resp Http403, s
